1518b5316076ba11b30731e60bce42fd5de0adc7
m-maheshreddy <81853497+m-maheshreddy@users.noreply.github.com>
2021-04-15 22:42:21 +0530

    Update WelcomeController.java

--- src/main/java/com/mahesh/web/controller/WelcomeController.java
+++ src/main/java/com/mahesh/web/controller/WelcomeController.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
 
 @Controller
+@RequestMapping("/hello")
 public class WelcomeController {
 
     private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
